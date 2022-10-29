04dc03fe2885728a87cd0e8aafef7d74023f3b37
osmak4real <osmak4real@gmail.com>
2022-10-29 20:32:14 +0100

    boolean changes

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/aboutme")
 def aboutme():
-    return {"slackUsername":"osmak", "backend":"true","age":32,"bio":"I am Nigerian and my dream is to become a full stack developer"}
+    return {"slackUsername":"osmak", "backend":True,"age":32,"bio":"I am Nigerian and my dream is to become a full stack developer"}
 
